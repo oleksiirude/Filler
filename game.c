9d26cd2794a3e147f8fa46e1d8bfd10954fa5f23
oleksiirude <oleksiirudenkomail@gmail.com>
2019-02-21 21:03:22 +0200

    simple shape inserting (processing)

--- game.c
+++ game.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "filler.h"
+#include <limits.h>
 
 void	lets_play(t_data *board, t_token *token)
 {
@@ -35,16 +36,15 @@ void	lets_play(t_data *board, t_token *token)
 	overlap = 0;
 	reply.y = 0;
 	reply.x = 0;
+	ft_printf("y[%d], x[%d]\n\n", token->y, token->x);
 	ft_printf("Player: %d\n", board->player);
-	ft_printf("Enemy: %d\n", board->enemy);
-	while (y < board->y)
+	ft_printf("Enemy: %d\n\n", board->enemy);
+	while (y + (token->y - 1) < board->y)
 	{
 		reply.y = y;
 		reply.x = x;
-		while (x < board->x)
+		while (x + (token->x - 1) < board->x)
 		{
-			reply.y = y;
-			reply.x = x;
 			while (t.y < token->y)
 			{
 				m.x = x;
@@ -53,23 +53,32 @@ void	lets_play(t_data *board, t_token *token)
 					if (board->map[m.y][m.x] == board->player && token->token[t.y][t.x] == '*')
 						overlap++;
 					else if (board->map[m.y][m.x] == board->enemy && token->token[t.y][t.x] == '*')
-						break ;
+					{
+						overlap = 0;
+						sign = 1;
+						break;
+					}
 					t.x++;
 					m.x++;
 				}
 				m.y++;
 				t.y++;
 				t.x = 0;
+				if (sign)
+					break ;
 			}
 			if (overlap == 1)
+			{
+				printf("Reply: y-%d, x->%d\n", y, x);
 				exit(0);
+			}
 			x++;
 			m.y = y;
 			t.y = 0;
+			overlap = 0;
 		}
-		printf("x->%d\n", x);
-		overlap = 0;
-		x = 0;
 		y++;
+		m.y = y;
+		x = 0;
 	}
 }
